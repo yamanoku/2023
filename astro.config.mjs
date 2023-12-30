2a9ddfabb4add4d1e2edcaeeef884da9186f3ccc
yamanoku <0910yama@gmail.com>
2023-12-30 20:41:45 +0900

    mdxを呼び出す設定＆rehypePluginsを簡易化

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,23 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
-const rehypePlugins = [
-  'remark-gfm',
-  [
-    'rehype-plugin-auto-resolve-layout-shift',
-    { type: 'maxWidth', maxWidth: 720 },
-  ],
-  'rehype-plugin-image-native-lazy-loading',
-];
+import mdx from '@astrojs/mdx';
+import resolveLayoutShiftPlugin from 'rehype-plugin-auto-resolve-layout-shift'
+import lazyloadPlugin from 'rehype-plugin-image-native-lazy-loading';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://2023.yamanoku.net',
-  integrations: [sitemap()],
   markdown: {
     syntaxHighlight: 'prism',
-    rehypePlugins,
+    gfm: true,
+    rehypePlugins: [
+      [resolveLayoutShiftPlugin, { type: 'maxWidth', maxWidth: 900 }],
+      lazyloadPlugin,
+    ],
     remarkRehype: {
       footnoteLabel: '脚注',
       footnoteBackLabel: 'コンテンツに戻る',
     },
   },
+  integrations: [sitemap(), mdx()],
 });
