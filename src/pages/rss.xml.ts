83bd5b13b4741861fe94f37f254d4ce8659ab92d
yamanoku <0910yama@gmail.com>
2024-12-14 15:02:40 +0900

    update

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,14 +2,16 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export async function get(context) {
-  const posts = await getCollection('articles');
+export async function GET(context) {
+  const posts = (await getCollection('articles')).sort(
+    (a, b) => new Date(a.data.date).valueOf() - new Date(b.data.date).valueOf(),
+  );
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map((post) => ({
-      link: post.slug,
+      link: post.id,
       title: post.data.title,
       pubDate: post.data.date,
     })),
