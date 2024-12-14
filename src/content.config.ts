ffdae826b2c6e0816deff95bfb856b66bc0753fc
yamanoku <0910yama@gmail.com>
2024-12-14 15:23:26 +0900

    move

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const articles = defineCollection({
-  loader: glob({ base: './src/content/articles', pattern: '**/*.{md,mdx}' }),
+  loader: glob({ base: './src/articles', pattern: '**/*.{md,mdx}' }),
   schema: z.object({
     date: z.date(),
     title: z.string(),
